a63d61d2d6d39db6f15fa6bfbce6ea297a5c514d
Everson Feijó <noob-dev@outlook.com>
2024-11-07 18:01:31 -0300

    Excluindo o menu, ficou desajeitado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
-import Menu from "./components/menu"
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
       <Header />
       <ListarTarefa />
+      
     </div>
   );
 }
