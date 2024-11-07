1b5795906b8819c87ad93431a0cd754d8e530a8a
EversonFeijo <noob-dev@outlook.com>
2024-11-07 16:32:23 -0300

    Update App.js
    importando o componente menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Menu from "./components/menu"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Header />
       <ListarTarefa />
     </div>
